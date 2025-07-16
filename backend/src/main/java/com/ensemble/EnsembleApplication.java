52e7a8dd2e60b3423d0d93f24041bad5edecdbb4
adrbessy <adrienbessy@hotmail.com>
2025-07-16 07:27:53 +0200

    Ajout de CrossOrigin

--- backend/src/main/java/com/ensemble/EnsembleApplication.java
+++ backend/src/main/java/com/ensemble/EnsembleApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ensemble")
 public class EnsembleApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnsembleApplication.class, args);
--- backend/src/main/java/com/ensemble/controller/PingController.java
+++ backend/src/main/java/com/ensemble/controller/PingController.java
@@ -1,8 +1,10 @@
 package com.ensemble.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PingController {
 
